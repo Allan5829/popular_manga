f34302b5bba056a405239f5963e48a10819aa700
Allan Farca <allan.farca@gmail.com>
2020-05-08 05:52:00 +0000

    setting up popular_manga.rb

--- lib/popular_manga.rb
+++ lib/popular_manga.rb
@@ -1,3 +1,11 @@
-
-
-puts "do I see this"
+class PopularManga
+  attr_accessible :name, :manga_url, :creator, :creator_info, :series_info, :recent_volume, :recent_volume_info #info I will be scraping
+  
+  @@all = []
+  
+  def initialize (name, manga_url)
+    @name = name 
+    @manga_url = manga_url
+    @@all << self 
+  end 
+end 
