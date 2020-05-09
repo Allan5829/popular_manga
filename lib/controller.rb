700967550c8766d3ce8bfedb82b2792d8f2facf8
Allan Farca <allan.farca@gmail.com>
2020-05-09 16:04:54 +0000

    fully implemented 2 layers deep with fake data

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,11 +2,11 @@ class Controller
   
   def start
     puts "Welcome to the Popular Manga CLI!"
+    MangaScraper.fake_scrape # call on a method name TBD to start scraping and get all objects needed in the PopularManga class 
     menu
   end 
   
   def menu
-    MangaScraper.fake_scrape # call on a method name TBD to start scraping and get all objects needed in the PopularManga class 
     puts "Please select the manga you would like to learn more about by typing 1-5"
     # call PopularManga.all and  display alphabetically 
     PopularManga.all.each_with_index do |manga, index|
@@ -32,15 +32,17 @@ class Controller
   
   def display_more_info (input)
     manga = PopularManga.all[input -1]
+    puts ""
     puts "What would you like to learn more about?"
     puts "Type 1 to learn more about the creator, type 2 to learn more about a recent volume, or type 0 to go back to the menu."
+    puts ""
     number = gets.strip
     more_input = number.to_i
-    if input == 0 
+    if more_input == 0 
       menu 
-    elsif input == 1 
+    elsif more_input == 1 
       puts "#{manga.creator_info} input is 1"
-    elsif input == 2 
+    elsif more_input == 2 
       puts "#{manga.creator_info} input is 2"
     else 
       invalid
