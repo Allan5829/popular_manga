02b503ee0648b0cf1ac98360f0612afc16d1960e
Allan Farca <allan.farca@gmail.com>
2020-05-09 15:58:57 +0000

    adding functionality to go 2 layers deep

--- lib/controller.rb
+++ lib/controller.rb
@@ -21,15 +21,34 @@ class Controller
   
   def display_info (input)
     total_manga = PopularManga.all.size
-    if (input > 0) && (input < total_manga)
+    if (input > 0) && (input <= total_manga)
       manga = PopularManga.all[input -1]
       puts "#{manga.creator}, #{manga.creator_info}"
+      display_more_info(input)
     else 
-      puts "That was not a valid input"
+      invalid
     end 
-    
-    
-    
-    
   end 
+  
+  def display_more_info (input)
+    manga = PopularManga.all[input -1]
+    puts "What would you like to learn more about?"
+    puts "Type 1 to learn more about the creator, type 2 to learn more about a recent volume, or type 0 to go back to the menu."
+    number = gets.strip
+    more_input = number.to_i
+    if input == 0 
+      menu 
+    elsif input == 1 
+      puts "#{manga.creator_info} input is 1"
+    elsif input == 2 
+      puts "#{manga.creator_info} input is 2"
+    else 
+      invalid
+    end 
+  end 
+  
+  def invalid
+    puts "That was not a valid input"
+  end 
+  
 end #what I'm going to do now is implement what I want my cli to show but "fake" the data for now 
